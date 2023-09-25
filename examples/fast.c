d0432f638c978b384dd8ca2502757c2ca787f127
Michael Mackus <michaelmackus@gmail.com>
2023-09-24 18:30:57 -0700

    Update example

--- examples/fast.c
+++ examples/fast.c
@@ -1,6 +1,7 @@
 #include <time.h>
 #include <SDL2/SDL.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 /**
  * Raw SDL2 pixel plotting example with surface blitting.
@@ -28,18 +29,8 @@ int main()
     SDL_Renderer *s = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED) ;
     SDL_GL_SetSwapInterval(0);
 
-    const int pointLocationx = windowWidth/2;
-    const int pointLocationy = windowHeight/2;
-
     int quit = 0;
     SDL_Event event;
-
-    //drawing particles
-    //setting up objects
-    //repeated over and over again
-
-    int msec, i;
-    clock_t start, diff;
 
     /* SDL_HasWindowSurface(); */
     SDL_Surface *window_surface = SDL_GetWindowSurface(window);
@@ -51,54 +42,59 @@ int main()
     SDL_RenderClear(s);
     // Set our color for the draw functions
     SDL_SetRenderDrawColor(s, 0xFF, 0xFF, 0xFF, 0xFF);
+    // show the window
+    SDL_RenderPresent(s);
 
-    /* i = 100000; */
-    start = clock();
-    SDL_LockSurface(surface);
-    unsigned int *color;
-    unsigned char r,g,b;
-    uint32_t *pixels;
-    pixels = (uint32_t*) surface->pixels;
-    r = 255;
-    g = 127;
-    b = 0;
-    i = 0;
-    for (int y=0; y<windowHeight; ++y) {
-        for (int x=0; x<windowWidth; ++x) {
-            pixels[i] = r << 24 | g << 16 | b << 8 | 0xFF;
-            ++r;
-            --g;
-            ++b;
-            ++i;
-        }
-    }
-    SDL_UnlockSurface(surface);
-
-    SDL_Rect rect;
-    rect.x = 0;
-    rect.y = 0;
-    rect.w = windowWidth;
-    rect.h = windowHeight;
-
-    SDL_BlitSurface(surface, &rect, window_surface, &rect);
-    SDL_UpdateWindowSurface(window);
-
-    diff = clock() - start;
-    msec = diff * 1000 / CLOCKS_PER_SEC;
-    printf("Time taken %d seconds %d milliseconds", msec/1000, msec%1000);
-
-    // Now we can draw our point
-    /* SDL_RenderDrawPoint(s, pointLocationx, pointLocationy); */
-    // Set the color to what was before
-    /* SDL_SetRenderDrawColor(s, 0x00, 0x00, 0x00, 0xFF); */
-    // .. you could do some other drawing here
-    // And now we present everything we draw after the clear.
-    /* SDL_RenderPresent(s); */
-
+    int generate = 1;
     while (!quit) {
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+            if (event.type == SDL_QUIT || (event.type == SDL_KEYUP && event.key.keysym.sym == SDLK_q)) {
                 quit = 1;
+            }
+
+            if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_RETURN) {
+                generate = 1;
+            }
+
+            if (generate) {
+                int msec, i;
+                clock_t start, diff;
+                unsigned char r,g,b;
+                uint32_t *pixels;
+
+                /* generate = 0; */
+                start = clock();
+
+                SDL_RenderClear(s);
+                SDL_LockSurface(surface);
+
+                pixels = (uint32_t*) surface->pixels;
+                r = rand() % 256;
+                g = rand() % 256;
+                b = rand() % 256;
+                for (int y=0; y<windowHeight; ++y) {
+                    for (int x=0; x<windowWidth; ++x) {
+                        pixels[x + y*windowWidth] = r << 24 | g << 16 | b << 8 | 0xFF;
+                        ++r;
+                        --g;
+                        b ^= b;
+                        ++b;
+                    }
+                }
+                SDL_UnlockSurface(surface);
+
+                SDL_Rect rect;
+                rect.x = 0;
+                rect.y = 0;
+                rect.w = windowWidth;
+                rect.h = windowHeight;
+
+                SDL_BlitSurface(surface, &rect, window_surface, &rect);
+                SDL_UpdateWindowSurface(window);
+
+                diff = clock() - start;
+                msec = diff * 1000 / CLOCKS_PER_SEC;
+                printf("Time taken %d seconds %d milliseconds\n", msec/1000, msec%1000);
             }
         }
 
